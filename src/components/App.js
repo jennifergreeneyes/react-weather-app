f8dd34361bee6e5aa9bc0812818ee2939d016688
Jennifer Cant <jcant6@gmail.com>
2021-02-04 08:58:58 +0000

    tidied files & made city appear in app

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,22 @@
 import React from "react";
 import "../styles/App.css";
+import PropTypes from "prop-types";
+import LocationDetails from "./LocationDetails";
 
-function App() {
+function App(props) {
+  const { location } = props;
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <LocationDetails city={location.city} country={location.country} />
     </div>
   );
 }
+
+App.propTypes = {
+  location: PropTypes.shape({
+    city: PropTypes.string,
+    country: PropTypes.string,
+  }).isRequired,
+};
 
 export default App;
