e75b7587ae7a3eafc8dad4330c0ed38887a55403
Jennifer Cant <jcant6@gmail.com>
2021-02-06 10:49:16 +0000

    forecast list rendering

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,27 @@ import React from "react";
 import "../styles/App.css";
 import PropTypes from "prop-types";
 import LocationDetails from "./LocationDetails";
+import ForecastSummaries from "./ForecastSummaries";
 
-function App(props) {
-  const { location } = props;
-  return (
-    <div className="App">
-      <LocationDetails city={location.city} country={location.country} />
-    </div>
-  );
-}
+const App = ({ forecasts, location }) => (
+  <div className="App">
+    <LocationDetails city={location.city} country={location.country} />
+    <ForecastSummaries forecasts={forecasts} />
+  </div>
+);
 
 App.propTypes = {
+  forecasts: PropTypes.arrayOf(
+    PropTypes.shape({
+      data: PropTypes.number,
+      description: PropTypes.string,
+      icon: PropTypes.number,
+      temperature: PropTypes.shape({
+        max: PropTypes.number,
+        min: PropTypes.number,
+      }),
+    })
+  ).isRequired,
   location: PropTypes.shape({
     city: PropTypes.string,
     country: PropTypes.string,
--- src/components/ForecastSummary.js
+++ src/components/ForecastSummary.js
@@ -6,9 +6,12 @@ const ForecastSummary = (props) => {
   return (
     <div className="forecast-summary">
       <div className="forecast-summary__date">{date}</div>
-      <div className="forecast-summary__icon" data-testid="forecast-icon">{icon}</div>
+      <div className="forecast-summary__icon" data-testid="forecast-icon">
+        {icon}
+      </div>
       <div className="forecast-summary__temperature">
-        {temperature.max}&deg;C
+        {temperature.max}
+        &deg;C
       </div>
       <div className="forecast-summary__description">{description}</div>
     </div>
