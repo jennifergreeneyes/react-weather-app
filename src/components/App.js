230f851443836693db3a18c21a2c43b596c0768c
Jennifer Cant <jcant6@gmail.com>
2021-02-09 21:45:16 +0000

    first state step

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import LocationDetails from "./LocationDetails";
 import ForecastSummaries from "./ForecastSummaries";
@@ -6,13 +6,19 @@ import ForecastDetails from "./ForecastDetails";
 
 import "../styles/App.css";
 
-const App = ({ forecasts, location }) => (
-  <div className="weather-app">
-    <LocationDetails city={location.city} country={location.country} />
-    <ForecastSummaries forecasts={forecasts} />
-    <ForecastDetails forecast={forecasts[0]} />
-  </div>
-);
+const App = ({ forecasts, location }) => {
+  const [selectedDate, setSelectedDate] = useState(forecasts[0].date);
+  const selectedForecast = forecasts.find(
+    (forecast) => forecast.date === selectedDate
+  );
+  return (
+    <div className="weather-app">
+      <LocationDetails city={location.city} country={location.country} />
+      <ForecastSummaries forecasts={forecasts} />
+      <ForecastDetails forecast={selectedForecast} />
+    </div>
+  );
+};
 
 App.propTypes = {
   forecasts: PropTypes.arrayOf(
